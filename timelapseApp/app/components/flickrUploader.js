cc0a8ba76709e356d89b4fa0aff6f92286f0d615
jmsaavedra <saavedra@jos.ph>
2016-02-03 07:13:43 -0500

    removed old auth secrets

--- timelapseApp/app/components/flickrUploader.js
+++ timelapseApp/app/components/flickrUploader.js
@@ -16,10 +16,10 @@ var Flickr = require("flickrapi");
 var FlickrOptions = {
   api_key: global.KEYS.FLICKR.key,
   secret: global.KEYS.FLICKR.secret,
-  permissions: "write",
-  user_id: global.KEYS.FLICKR.user_id,
-  access_token: global.KEYS.FLICKR.access_token,
-  access_token_secret: global.KEYS.FLICKR.access_token_secret
+  permissions: "write"
+  // user_id: global.KEYS.FLICKR.user_id,
+  // access_token: global.KEYS.FLICKR.access_token,
+  // access_token_secret: global.KEYS.FLICKR.access_token_secret
 };
 
 /***
